d63d1cc9b5b88feb36a3c4b6af7041b3576ba0bd
samervalente <samervalente@gmail.com>
2023-03-17 14:44:55 -0300

    feat: create guessing game

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut username = String::new();
+    let mut age = String::new();
+    let mut guess = String::new();
+
+    println!("Please input your user name!");
+
+    io::stdin()
+        .read_line(&mut username)
+        .expect("Failed to read user name read line");
+
+    println!("Hello {username}. How old are you?");
+    io::stdin()
+        .read_line(&mut age)
+        .expect("Failed to read age input read line");
+
+    println!("Nice! Now input your guess. Good Luck!");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please, type a number!");
+
+    println!(
+        "Result: \n username:{} age:{age} guessed: {}, secret_number: {secret_number}",
+        username, guess
+    );
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!!"),
+    }
+}
